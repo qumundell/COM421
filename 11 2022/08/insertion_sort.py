337326cfcfefdd31801049cad5f4cb461ddac97a
6mundq94 <6mundq94@solent.ac.uk>
2022-11-08 13:47:01 +0000

    Finished recursive insertion sort

---
+++ 11 2022/08/insertion_sort.py
@@ -0,0 +1,18 @@
+def sort(nums, divider=0):
+    if divider == 0:
+        sort(nums, divider + 1)
+
+    elif nums[divider - 1] > nums[divider]:
+        tempval = nums[divider]
+        nums[divider] = nums[divider - 1]
+        nums[divider - 1] = tempval
+        for i in range(divider):
+            sort(nums, i)
+
+    if divider != len(nums) - 1:
+        sort(nums, divider + 1)
+
+
+list = [17, 6, 23, 4, 28, 30, 54, 28, 83, 7]
+sort(list)
+print(list)
