1344b35eb45d0e7c8c0c14cf7288cc45e1c00038
qumundell <6mundq94@solent.ac.uk>
2022-11-22 15:29:48 +0000

    Updated to be more efficient

--- 11 2022/22/mergesort.py
+++ 11 2022/22/mergesort.py
@@ -4,11 +4,9 @@ def mergesort(numbers):
     list1 = [numbers[i] for i in range(midpoint)]
     if len(list1) > 1:
         list1 = mergesort(list1)
-        print(list1)
     list2 = [numbers[i] for i in range(midpoint, len(numbers))]
     if len(list2) > 1:
         list2 = mergesort(list2)
-        print(list2)
     mergedlist = merge(list1, list2)
     print(mergedlist)
     return mergedlist
@@ -18,25 +16,25 @@ def merge(list1, list2):
     counter2 = 0
     sortedList = []
     while counter1 < len(list1) and counter2 < len(list2):
-        if list1[counter1] < list2[counter2]:
+        if list1[counter1] <= list2[counter2]:
             sortedList.append(list1[counter1])
             counter1 += 1
-        elif list2[counter2] < list1[counter1]:
+        else: # elif list2[counter2] < list1[counter1]:
             sortedList.append(list2[counter2])
             counter2 += 1
-        else:
-            sortedList.append(list1[counter1])
-            sortedList.append(list2[counter2])
-            counter1 +=1
-            counter2 +=1
+
+
     if counter1 < len(list1):
+        selected_list = list1
+
         for i in range(counter1, len(list1)):
             sortedList.append(list1[i])
     elif counter2 < len(list2):
+        selected_list = list2
         for i in range(counter2, len(list2)):
             sortedList.append(list2[i])
     return sortedList
 
-number = [22, 56, 1, 59, 38, 7, 15, 17, 33]
+number = [22, 56, 59, 1, 38, 7, 15, 17, 33]
 number = mergesort(number)
 print(number)
